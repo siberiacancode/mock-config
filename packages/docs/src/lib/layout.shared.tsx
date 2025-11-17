133684bd010ba577cab9ac0469ee59122b4a5abb
Camon-fi07 <422nikitos244@mail.ru>
2025-11-17 22:50:47 +0700

    #244 👽 change title of docs

--- packages/docs/src/lib/layout.shared.tsx
+++ packages/docs/src/lib/layout.shared.tsx
@@ -15,7 +15,7 @@ export function baseOptions(): BaseLayoutProps {
           <svg aria-label='Logo' height='24' width='24' xmlns='http://www.w3.org/2000/svg'>
             <circle cx={12} cy={12} fill='currentColor' r={12} />
           </svg>
-          My App
+          Mock config server
         </>
       )
     },
