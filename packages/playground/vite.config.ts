ae86f912df77f400048543a09a1b462d3ad9fdb8
debabin <dmitriybabinup@gmail.com>
2026-03-23 13:49:39 +0700

    feature/playground 🧊 fix tests for playground

---
+++ packages/playground/vite.config.ts
@@ -0,0 +1,18 @@
+import { vitest } from '@siberiacancode/vitest';
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+
+export default defineConfig({
+  test: {
+    ...vitest,
+    environment: 'node'
+  },
+  resolve: {
+    alias: {
+      
+      '@/shared': path.resolve(__dirname, '../shared'),
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
