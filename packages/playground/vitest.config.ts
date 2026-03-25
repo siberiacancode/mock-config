261df15769afaf2e156920d5dff074f1bd1eade8
debabin <dmitriybabinup@gmail.com>
2026-03-25 21:18:31 +0700

    feature/playground 🧊 name to vitest

--- packages/playground/vitest.config.ts
+++ packages/playground/vitest.config.ts
@@ -2,7 +2,6 @@ import { vitest } from '@siberiacancode/vitest';
 import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
-
 export default defineConfig({
   test: {
     ...vitest,
@@ -10,7 +9,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      
       '@/shared': path.resolve(__dirname, '../shared'),
       '@': path.resolve(__dirname, './src')
     }
--- packages/shared/vitest.config.ts
+++ packages/shared/vitest.config.ts
@@ -1,8 +1,6 @@
 import { vitest } from '@siberiacancode/vitest';
-import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
-console.log('@', path.resolve(__dirname, '.'));
 export default defineConfig({
   test: {
     ...vitest,
