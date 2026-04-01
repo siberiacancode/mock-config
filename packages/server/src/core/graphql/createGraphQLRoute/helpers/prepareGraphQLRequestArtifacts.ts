b5172016a935f8c9ded139279b7df371e3ddf6e4
debabin <dmitriybabinup@gmail.com>
2026-04-01 20:45:26 +0700

    fix/params 🧊 fix path for graphql

--- packages/server/src/core/graphql/createGraphQLRoute/helpers/prepareGraphQLRequestArtifacts.ts
+++ packages/server/src/core/graphql/createGraphQLRoute/helpers/prepareGraphQLRequestArtifacts.ts
@@ -18,17 +18,17 @@ export const matchGraphQLRequestArtifacts = ({
   meta
 }: MatchGraphQLRequestArtifactsParams) =>
   artifacts.filter((artifact) => {
-    if (!meta.path.startsWith(artifact.baseUrl)) return false;
+    if (meta.path !== artifact.baseUrl) return false;
 
     if (artifact.operationType !== meta.operationType) return false;
 
     if (artifact.query) {
-      return artifact.query.replace(/\s+/g, '') === meta.query?.replace(/\s+/g, '');
+      if (!meta.query) return false;
+      return artifact.query.replace(/\s+/g, '') === meta.query.replace(/\s+/g, '');
     }
 
     if (artifact.operationName) {
       if (!meta.operationName) return false;
-
       return artifact.operationName instanceof RegExp
         ? new RegExp(artifact.operationName).test(meta.operationName)
         : artifact.operationName === meta.operationName;
