6672251b115cdffc9bdd23a3c45f1740837a63d5
zeroqs <smthq@bk.ru>
2026-07-18 03:12:23 +0300

    fix: update inspector CLI open flag type

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -1,9 +1,8 @@
 import type { Arguments } from 'yargs';
 
 export type MockServerInspectorArgv = Arguments<{
-  baseUrl?: string;
   port?: number;
   config?: string;
   host?: string;
-  open?: string;
+  open?: boolean;
 }>;
