532ac2e489b9657a700ea155476081aae61adbcb
debabin <dmitriybabinup@gmail.com>
2026-07-19 19:26:13 +0700

    docsrework 🎉 add new match, new blocks

--- packages/docs/src/components/icons/LogoIcon.tsx
+++ packages/docs/src/components/icons/LogoIcon.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export const LogoIcon = () => (
-  <Image alt='logo' height={25} src='/mock-config/icons/mock-config-logo.svg' width={25} />
+  <Image alt='logo' height={23} src='/mock-config/icons/mock-config-logo.svg' width={23} />
 );
--- packages/docs/src/lib/layout.shared.tsx
+++ packages/docs/src/lib/layout.shared.tsx
@@ -3,11 +3,12 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { LogoIcon } from '@/components/icons';
 
 export const baseOptions = (): BaseLayoutProps => ({
+  githubUrl: 'https://github.com/siberiacancode/mock-config',
   nav: {
     title: (
       <>
         <LogoIcon />
-        <span className='text-xl'>mock-config</span>
+        <span className='text-lg'>Mock config</span>
       </>
     )
   },
