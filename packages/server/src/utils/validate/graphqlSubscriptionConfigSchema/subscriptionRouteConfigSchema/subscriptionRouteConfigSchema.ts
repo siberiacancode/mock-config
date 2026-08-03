bdc67d9bcac9f88cb21eb88757b086e0e9245ac9
zeroqs <smthq@bk.ru>
2026-08-03 04:49:05 +0300

    #290 🎉 fix: allow settings on graphql subscription routes

--- packages/server/src/utils/validate/graphqlSubscriptionConfigSchema/subscriptionRouteConfigSchema/subscriptionRouteConfigSchema.ts
+++ packages/server/src/utils/validate/graphqlSubscriptionConfigSchema/subscriptionRouteConfigSchema/subscriptionRouteConfigSchema.ts
@@ -4,10 +4,15 @@ import { isPlainObject } from '@/utils/helpers';
 
 import { plainObjectSchema, variablesEntitySchema } from '../../utils';
 
+const subscriptionSettingsSchema = z.strictObject({
+  delay: z.number().nonnegative().optional()
+});
+
 export const subscriptionRouteConfigSchema = z
   .custom((value) => isPlainObject(value) && 'data' in value)
   .pipe(
     z.strictObject({
+      settings: plainObjectSchema(subscriptionSettingsSchema).optional(),
       data: z.union([z.function(), z.any()]),
       entities: plainObjectSchema(
         z.strictObject({
